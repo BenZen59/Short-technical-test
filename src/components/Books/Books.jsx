b879ced1b4607bd2688c0f50e948b0fd105a2824
BenZen59 <benoit.zenone@gmail.com>
2022-08-02 15:56:42 +0200

    add button filter by levels

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -24,35 +24,72 @@ const BOOKS_QUERY = gql`
 
 export default function Books() {
   const [showChapters, setShowChapters] = useState('');
+  const [subjectsBooks, setSubjectsBooks] = useState('');
+  const { loading, error, data } = useQuery(BOOKS_QUERY);
+
   function handleChapter(id) {
     setShowChapters(id);
   }
-  const { loading, error, data } = useQuery(BOOKS_QUERY);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
+  return (
+    <div>
+      <span className='levels'>Classement par niveaux</span>
+      <button type='button' onClick={() => setSubjectsBooks('6ème')}>
+        6ème
+      </button>
+      <button type='button' onClick={() => setSubjectsBooks('5ème')}>
+        5ème
+      </button>
+      <button type='button' onClick={() => setSubjectsBooks('4ème')}>
+        4ème
+      </button>
+      <button type='button' onClick={() => setSubjectsBooks('3ème')}>
+        3ème
+      </button>
+      <button type='button' onClick={() => setSubjectsBooks('2de')}>
+        2de
+      </button>
+      <button type='button' onClick={() => setSubjectsBooks('1re')}>
+        1re
+      </button>
+      <button type='button' onClick={() => setSubjectsBooks('1re Bac Pro')}>
+        1re Bac Pro
+      </button>
+      <button type='button' onClick={() => setSubjectsBooks('Terminale')}>
+        Terminale
+      </button>
+      <button
+        type='button'
+        onClick={() => setSubjectsBooks('Terminale Bac Pro')}
+      >
+        Terminale Bac Pro
+      </button>
+      {data.viewer.books.hits
+        .filter(
+          ({ valid, levels }) =>
+            valid === true && levels[0].name.includes(subjectsBooks)
+        )
+        .sort((a, b) => (a.subjects.name > b.subjects.name ? 1 : -1))
+        .map(({ id, displayTitle, url }) => (
+          <div key={id}>
+            <h3>{displayTitle}</h3>
+            <img src={url} alt='cover' className='cover' />
+            <br />
 
-  return data.viewer.books.hits
-    .filter(({ valid }) => valid === true)
-    .sort((a, b) =>
-      a.subjects.name > b.subjects.name
-        ? 1
-        : -1 && a.levels.name > b.levels.name
-        ? 1
-        : -1
-    )
-    .map(({ id, displayTitle, url }) => (
-      <div key={id}>
-        <h3>{displayTitle}</h3>
-        <img src={url} alt='cover' className='cover' />
-        <br />
+            <button id={id} type='button' onClick={(e) => handleChapter(id, e)}>
+              Montrer les chapitres
+            </button>
+            {showChapters === id && <Chapters bookId={id} />}
 
-        <button id={id} type='button' onClick={(e) => handleChapter(id, e)}>
-          Montrer les chapitres
-        </button>
-        {showChapters === id && <Chapters bookId={id} />}
-
-        <br />
-      </div>
-    ));
+            <br />
+          </div>
+        ))}
+    </div>
+  );
 }
+
+// && a.levels.name > b.levels.name
+//         ? 1
+//         : -1
